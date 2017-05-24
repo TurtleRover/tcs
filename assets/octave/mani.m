f852c557a39c1cfc53a7895f12ebb7d494b2e729
Marcin Twardak <twardakm@gmail.com>
2017-05-24 18:43:43 +0200

    better mesh

--- assets/octave/mani.m
+++ assets/octave/mani.m
@@ -1,50 +1,50 @@
 clear
 
 # manipulator constants
-a = 144.5
-b = 220
+a = 144.5;
+b = 220;
 
 # generate angles in radians
-res = 100
-alpha = linspace(20, 135, res)
-beta = linspace(90, 225, res)
+res = 250;
+alpha = linspace(20, 135, res);
+beta = linspace(90, 225, res);
 
-x = zeros(res,res)
-y = zeros(res,res)
+x = zeros(res,res);
+y = zeros(res,res);
 
-[i,j] = meshgrid(1:numel(alpha),1:numel(beta))
-x = a * cosd(alpha(i)) + b * cosd(203.5 - beta(j))
-y = a * sind(alpha(i)) - b * sind(203.5 - beta(j))
+[i,j] = meshgrid(1:numel(alpha),1:numel(beta));
+x = a * cosd(alpha(i)) + b * cosd(203.5 - beta(j));
+y = a * sind(alpha(i)) - b * sind(203.5 - beta(j));
 
 # generate linear approximation
-res = 25
-x_lin = linspace(120, 300, res)
-y_lin = linspace(-150, 200, res)
-alpha_angles = zeros(res,res)
-beta_angles = zeros(res,res)
+res = 100;
+x_lin = linspace(120, 300, res);
+y_lin = linspace(-150, 200, res);
+alpha_angles = zeros(res,res);
+beta_angles = zeros(res,res);
 
-k = linspace(1, numel(alpha), numel(alpha))
-l = linspace(1, numel(beta), numel(beta))
+k = linspace(1, numel(alpha), numel(alpha));
+l = linspace(1, numel(beta), numel(beta));
 
-cost = zeros(numel(alpha),numel(beta))
+cost = zeros(numel(alpha),numel(beta));
 
 for i = 1:numel(x_lin)
   for j = 1:numel(y_lin)
     
-    cost = (x(k,l) - x_lin(i)).^2 + (y(k,l) - y_lin(j)).^2
+    cost = (x(k,l) - x_lin(i)).^2 + (y(k,l) - y_lin(j)).^2;
     
-    minimum = min(cost(:))
-    [row, col] = find(cost==minimum)
+    minimum = min(cost(:));
+    [row, col] = find(cost==minimum);
     
-    alpha_angles(i,j) = alpha(col)
-    beta_angles(i,j) = beta(row)
+    alpha_angles(i,j) = alpha(col);
+    beta_angles(i,j) = beta(row);
     
   endfor
 endfor
 
-save("inverse kinematics.txt", "alpha_angles", "beta_angles", "x_lin", "y_lin")
+save("inverse kinematics.txt", "alpha_angles", "beta_angles", "x_lin", "y_lin");
 
-dlmwrite("alpha.csv", alpha_angles, "delimiter", ",", "precision", 3, "newline", "],\n[", "roffset",1)
-dlmwrite("beta.csv", beta_angles, "delimiter", ",", "precision", 3, "newline", "],\n[", "roffset",1)
-dlmwrite("x_lin.csv", x_lin, "delimiter", ",", "precision", 3, "newline", "],\n[", "roffset",1)
-dlmwrite("y_lin.csv", y_lin, "delimiter", ",", "precision", 3, "newline", "],\n[", "roffset",1)
+dlmwrite("alpha.csv", alpha_angles, "delimiter", ",", "precision", 3, "newline", "],\n[", "roffset",1);
+dlmwrite("beta.csv", beta_angles, "delimiter", ",", "precision", 3, "newline", "],\n[", "roffset",1);
+dlmwrite("x_lin.csv", x_lin, "delimiter", ",", "precision", 3, "newline", "],\n[", "roffset",1);
+dlmwrite("y_lin.csv", y_lin, "delimiter", ",", "precision", 3, "newline", "],\n[", "roffset",1);
