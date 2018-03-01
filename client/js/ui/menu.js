2f3c71c9e4622070dbbcf77300f497dd10202f00
stsdc <s.dac@yandex.ru>
2018-03-02 00:16:53 +0100

    breakpoints; logo in statubar for small screens

---
+++ client/js/ui/menu.js
@@ -0,0 +1,16 @@
+(function() {
+
+	var hamburger = {
+		navToggle: document.querySelector('.nav-toggle'),
+		nav: document.querySelector('nav'),
+
+		doToggle: function(e) {
+			e.preventDefault();
+			this.navToggle.classList.toggle('expanded');
+			this.nav.classList.toggle('expanded');
+		}
+	};
+
+	hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+
+}());
