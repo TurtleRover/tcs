d1fed65058860d23bb452792e17161eb47913c11
Stanisław <s.dac@yandex.ru>
2018-03-23 18:04:28 +0100

    socketio

--- assets/python/server/server.new.py
+++ assets/python/server/server.new.py
@@ -1,4 +1,28 @@
-# from sockets import sockets
-# from server.log import logger
+import asyncio
+from aiohttp import web
+import socketio
+import hexdump
+sio =  socketio.AsyncServer(async_mode='aiohttp',
+                           engineio_logger=True,
+                           logger=True)
+app = web.Application()
+sio.attach(app)
 
-import sockets
+
+@sio.on('connect')
+async def test_connect(sid, environ):
+    print ("connect")
+
+
+@sio.on('motors')
+async def test_message(sid, payload):
+    print(payload)
+    if payload[0] == 0x10:
+        print ("motors")
+    if payload[0] == 0x30:
+        print ("battery")
+
+
+if __name__ == '__main__':
+    # sio.start_background_task(background_task)
+    web.run_app(app, host='0.0.0.0', port=5000)
--- assets/python/server/sockets.py
+++ assets/python/server/sockets.py
@@ -1,41 +1,6 @@
-# from sockets import sockets
-# from server.log import logger
+from flask_socketio import emit
+from server.new import socketio
 
-import asyncio
-import datetime
-import random
-import websockets
-
-import socket
-import fcntl
-import struct
-
-import logging
-logger = logging.getLogger('websockets')
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
-# use network manager
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', bytes(ifname[:15], 'utf-8'))
-    )[20:24])
-
-async def time(websocket, path):
-    while True:
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
-        await websocket.send(now)
-        await asyncio.sleep(random.random() * 3)
-
-async def onConnect(self, request):
-    print("Client connecting: {0}".format(request.peer))
-
-async def onOpen(self):
-    print("WebSocket connection open.")
-
-start_server = websockets.serve(time, get_ip_address('wlan0'), 8080)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+@socketio.on('connection')
+def connection():
+    print("CONNECT")
