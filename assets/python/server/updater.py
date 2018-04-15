c10e1a919f55c9e07a01020cb6e2263dafcb5fb0
Stanisław <s.dac@yandex.ru>
2018-04-16 00:50:39 +0200

    get version from spawning external process of turtle server

--- assets/python/server/updater.py
+++ assets/python/server/updater.py
@@ -1,10 +1,22 @@
-from github import Github
-import requests
-gh = Github()
+# from github import Github
+# import requests
+import subprocess
 
-organization = gh.get_organization("TurtleRover")
-repository = organization.get_repo("Turtle-Rover-Mission-Control")
-latest = repository.get_latest_release()
+# gh = Github()
+#
+# organization = gh.get_organization("TurtleRover")
+# repository = organization.get_repo("Turtle-Rover-Mission-Control")
+# latest = repository.get_latest_release()
 
-print (latest.tarball_url)
-subprocess.call(["turtle", "-v"])
+# print (latest.tarball_url)
+
+def get_current_release():
+    return subprocess.run(['turtle', '-v'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+def convert_to_tuple(version):
+    return (int(version[0]), int(version[2]), int(version[4]))
+
+
+version = get_current_release()
+
+print (convert_to_tuple(version))
+# print (version)
