0fd786708252df552605be0f423b0860c8bc1ccb
Stanisław <s.dac@yandex.ru>
2018-04-17 01:34:23 +0200

    add exceptions in link_quality

--- assets/python/server/link_quality.py
+++ assets/python/server/link_quality.py
@@ -30,12 +30,17 @@ class Signal(Thread):
         self.start_counting = False
 
         self.i = 0
-
-        self.process = subprocess.Popen(['ping', '-i', '0.05', self.getIP(), '-f'], stdout=subprocess.PIPE)
+        self.process = None
 
     def getIP(self, ifname="wlan0"):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', bytes(ifname[:15], 'utf-8')) )[20:24])
+        ip = None
+        try:
+            ip = fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', bytes(ifname[:15], 'utf-8')) )[20:24]
+            return socket.inet_ntoa(ip)
+        except OSError as e:
+            logger.error('%s: %s at %s', e, ip, ifname)
+            return None
 
     def fillList(self, lenght, val):
         arr = []
@@ -43,32 +48,43 @@ class Signal(Thread):
             arr.append(val)
         return arr
 
+    def spawn_ping(self):
+        try:
+            ip = self.getIP()
+            self.process = subprocess.Popen(['ping', '-i', '0.05', ip, '-f'], stdout=subprocess.PIPE)
+        except BaseException:
+            logger.error('ping -i 0.05 %s -f', ip)
+
     def run(self):
-        while True:
-            output = self.process.stdout.read(1)
-            if output.decode('utf-8') == '' and self.process.poll() is not None:
-                break
-            if output:
-                self.prev_output_hex = self.output_hex
-                self.output_hex = hex(ord(output))
-                if self.output_hex == hex(0x08) and self.prev_output_hex == hex(0x2e):
-                    self.backspaces.pop(0)
-                    self.backspaces.append(1)
-                elif self.output_hex == hex(0x2e):
-                    if self.prev_output_hex == hex(0xa):
-                        self.start_counting = True
-                    if self.start_counting == True and self.prev_output_hex == hex(0x2e):
+        self.spawn_ping()
+        try:
+            while True:
+                output = self.process.stdout.read(1)
+                if output.decode('utf-8') == '' and self.process.poll() is not None:
+                    break
+                if output:
+                    self.prev_output_hex = self.output_hex
+                    self.output_hex = hex(ord(output))
+                    if self.output_hex == hex(0x08) and self.prev_output_hex == hex(0x2e):
                         self.backspaces.pop(0)
-                        self.backspaces.append(0)
-                    if self.start_counting == True:
-                        self.dots.pop(0)
-                        self.dots.append(1)
-                elif self.output_hex == hex(0xa):
-                    self.start_counting = False
+                        self.backspaces.append(1)
+                    elif self.output_hex == hex(0x2e):
+                        if self.prev_output_hex == hex(0xa):
+                            self.start_counting = True
+                        if self.start_counting == True and self.prev_output_hex == hex(0x2e):
+                            self.backspaces.pop(0)
+                            self.backspaces.append(0)
+                        if self.start_counting == True:
+                            self.dots.pop(0)
+                            self.dots.append(1)
+                    elif self.output_hex == hex(0xa):
+                        self.start_counting = False
 
-                self.i += 1
+                    self.i += 1
 
-                if (self.i >= self.MOVING_AVERAGE_LEN):
-                    self.strength = sum(self.backspaces) / sum(self.dots) * 100
-                    # logger.debug("link quality: %d", self.strength)
-                    self.i = 0
+                    if (self.i >= self.MOVING_AVERAGE_LEN):
+                        self.strength = sum(self.backspaces) / sum(self.dots) * 100
+                        # logger.debug("link quality: %d", self.strength)
+                        self.i = 0
+        except:
+            logger.error('Signal thread not started')
