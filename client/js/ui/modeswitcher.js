6c5a02af4f7b2b4d3ec2454ed1726167eeb23cf6
Stanisław <s.dac@yandex.ru>
2018-05-23 15:31:25 +0200

    mode switcher works; style manipulator sliders

--- client/js/ui/modeswitcher.js
+++ client/js/ui/modeswitcher.js
@@ -7,12 +7,13 @@
 
     function switch_mode(event) {
         let joystick = document.querySelector('.joystick');
+        let mani_control = document.querySelector('.manipulatorControl');
         if (event.target.value === 'drive') {
-            console.log('DRIVE');
+            mani_control.classList.add('manipulatorControl-hide');
             joystick.classList.remove('joystick-hide');
         } else if (event.target.value === 'grab') {
-            console.log('GRAB');
             joystick.classList.add('joystick-hide');
+            mani_control.classList.remove('manipulatorControl-hide');
         }
     }
 })();
