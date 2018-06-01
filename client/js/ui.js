28038a9aef4f2d8c642c1393602fcdd985e2734c
Stanisław <s.dac@yandex.ru>
2018-06-01 17:28:30 +0200

    show indicators icons dynamically

--- client/js/ui.js
+++ client/js/ui.js
@@ -536,11 +536,11 @@ var ui = (function() {
     });
 
     function setBatteryLevel(level) {
-        if (level == 4) $("#battery-level-indicator-img").attr('src', 'assets/img/ui/nav-bar-battery.svg');
-        else if (level == 3) $("#battery-level-indicator-img").attr('src', 'assets/img/ui/nav-bar-battery-3.svg');
-        else if (level == 2) $("#battery-level-indicator-img").attr('src', 'assets/img/ui/nav-bar-battery-2.svg');
-        else if (level == 1) $("#battery-level-indicator-img").attr('src', 'assets/img/ui/nav-bar-battery-1.svg');
-        else $("#battery-level-indicator-img").attr('src', 'assets/img/ui/nav-bar-battery-0.svg');
+        if (level == 4) $("#indicator-battery").attr('src', 'client/img/ui/battery-4.svg');
+        else if (level == 3) $("#indicator-battery").attr('src', 'client/img/ui/battery-3.svg');
+        else if (level == 2) $("#indicator-battery").attr('src', 'client/img/ui/battery-2.svg');
+        else if (level == 1) $("#indicator-battery").attr('src', 'client/img/ui/battery-1.svg');
+        else $("#battery-level-indicator-img").attr('src', 'client/img/ui/battery-0.svg');
     }
 
 
