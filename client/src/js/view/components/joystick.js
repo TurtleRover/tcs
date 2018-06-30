6a1a38e872a52647a9ef5e4f7cf7195f335c54fb
Stanisław <s.dac@yandex.ru>
2018-06-30 19:45:01 +0200

    remove space

--- client/src/js/view/components/joystick.js
+++ client/src/js/view/components/joystick.js
@@ -1,7 +1,7 @@
 import { h } from 'hyperapp'
 
 
-export const Joystick = ()  => 
+export const Joystick = () => 
     <div class="joystick">
         <canvas id="navigation-ring-canvas"></canvas>
         <img class="joystick_image" src={require("../../../img/ui/right-krzyz.svg")}/>
