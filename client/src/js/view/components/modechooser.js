3d68b676b866b9ea15daea3d84655f3ef4bed825
Stanisław <s.dac@yandex.ru>
2018-07-10 16:44:45 +0200

    rename modechooser. add min-width control-box-left

--- client/src/js/view/components/modechooser.js
+++ client/src/js/view/components/modechooser.js
@@ -2,7 +2,7 @@ import { h } from 'hyperapp'
 import { RadioButton } from "./elements/radio-button";
 
 export const ModeChooser = ({mode, setMode})  => 
-    <div class="mode-switcher">
+    <div class="mode-chooser">
         <RadioButton group='mode-switch' state={mode} value='grab' text='GRAB' setValue={setMode}/>
         <RadioButton group='mode-switch' state={mode} value='drive' text='DRIVE' setValue={setMode}/>
     </div>
