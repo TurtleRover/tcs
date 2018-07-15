aff9982e554e882d9337c75718f8435d9b8fea5e
Stanisław <s.dac@yandex.ru>
2018-07-15 21:53:18 +0200

    show joystick; drop canvas

--- client/src/js/view/components/joystick.js
+++ client/src/js/view/components/joystick.js
@@ -1,8 +1,34 @@
 import { h } from 'hyperapp'
-
+import nipplejs from 'nipplejs'
 
 export const Joystick = ({mode}) => 
-    <div class={(mode==='drive') ? 'joystick' : 'joystick joystick-hide'}>
-        <canvas id="navigation-ring-canvas"></canvas>
+    <div class={(mode==='drive') ? 'joystick' : 'joystick joystick-hide'} oncreate={(element) => attach_joystick(element)}>
         <img class="joystick_image" src={require("../../../img/ui/right-krzyz.svg")}/>
     </div>
+
+
+export const attach_joystick = function(element) {
+    let manager = nipplejs.create({
+        zone: element,
+        mode: 'static',
+        // color: 'rgba(0,0,0,0)',
+        position: {left: '50%', top: '50%'},
+        size: element.clientHeight
+    });
+
+      
+    manager.on('start', function(evt, nipple) {
+        console.log(evt);
+        nipple.on('move', function(evt, data) {
+            console.log(data);
+            
+
+        });
+    }).on('end', function(evt, nipple) {   
+        console.log(evt);
+        // nipple.off('start move end dir plain');
+    });
+}
+
+
+
