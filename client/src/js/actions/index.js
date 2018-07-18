16c3ceee83f4e011577b11b9064bdd17048d0ad0
Stanisław <s.dac@yandex.ru>
2018-07-18 15:01:09 +0200

    move joystick logic from view to actions

--- client/src/js/actions/index.js
+++ client/src/js/actions/index.js
@@ -1,5 +1,6 @@
 import nipplejs from 'nipplejs'
- 
+import {throttle} from 'lodash'
+
 const actions = {
   setBootScreenState: value => state => ({ showBootScreen: value }),
   setMode: value => state => ({ mode: value }),
@@ -16,8 +17,53 @@ const actions = {
   },
 
 
-  motors: null
+  motors: null,
   
+
+  joystick: ({element, motors}) => {
+    
+    let manager = nipplejs.create({
+    zone: element,
+    mode: 'static',
+    position: {left: '50%', top: '50%'},
+    size: element.clientHeight,
+    dataOnly: true
+    });
+    
+    manager.on('start', function (evt, nipple) {
+        console.log(evt);
+        nipple.on('move', (evt, data) => motorsThrottled(evt, data));
+    });
+
+    let motorsThrottled = throttle((evt, data) => {
+        if (!data.hasOwnProperty('direction')) { return; }
+        motors.set(treshold(data.force), convertToArrOfDirections(data.direction.angle));
+        console.log(Math.round(data.force*100), convertToArrOfDirections(data.direction.angle));        
+    }, 100, { 'trailing': false });
+
+    let treshold = (force) => force >= 1 ? 100 : (force * 100).toFixed(0);
+    
+    manager.on('end', function(evt, nipple) {   
+        console.log(evt);
+        // nipple.off('start move end dir plain');
+        motors.stop();
+    });
+
+    const convertToArrOfDirections = (angle) => {
+        switch (angle) {
+            case 'up':
+                return motors.direction.forward;
+            case 'down':
+                return motors.direction.backward;
+            case 'left':
+                return motors.direction.left;
+            case 'right':
+                return motors.direction.right;
+            default:
+                break;
+        }
+    }
+  }
 }
 
 export default actions
--- client/src/js/view/components/joystick.js
+++ client/src/js/view/components/joystick.js
@@ -1,53 +1,6 @@
 import { h } from 'hyperapp'
-import nipplejs from 'nipplejs'
-import {throttle} from 'lodash'
 
-export const Joystick = ({mode, motors}) =>
-    <div class={(mode==='drive') ? 'joystick' : 'joystick joystick-hide'} oncreate={(el) => joystick(el, motors)}>
+export const Joystick = ({mode, joystick, motors}) =>
+    <div class={(mode==='drive') ? 'joystick' : 'joystick joystick-hide'} oncreate={(element) => joystick({element, motors})}>
         <img class='joystick_image' src={require('../../../img/ui/right-krzyz.svg')}/>
     </div>
-
-
-const joystick = (element, motors) => {
-    let manager = nipplejs.create({
-    zone: element,
-    mode: 'static',
-    position: {left: '50%', top: '50%'},
-    size: element.clientHeight,
-    dataOnly: true
-    });
-    
-    manager.on('start', function (evt, nipple) {
-        console.log(evt);
-        nipple.on('move', (evt, data) => motorsThrottled(evt, data));
-    });
-
-    let motorsThrottled = throttle((evt, data) => {
-        if (!data.hasOwnProperty('direction')) { return; }
-        motors.set(treshold(data.force), convertToArrOfDirections(data.direction.angle));
-        console.log(Math.round(data.force*100), convertToArrOfDirections(data.direction.angle));        
-    }, 100, { 'trailing': false });
-
-    let treshold = (force) => force >= 1 ? 100 : (force * 100).toFixed(0);
-    
-    manager.on('end', function(evt, nipple) {   
-        console.log(evt);
-        // nipple.off('start move end dir plain');
-        motors.stop();
-    });
-
-    const convertToArrOfDirections = (angle) => {
-        switch (angle) {
-            case 'up':
-                return motors.direction.forward;
-            case 'down':
-                return motors.direction.backward;
-            case 'left':
-                return motors.direction.left;
-            case 'right':
-                return motors.direction.right;
-            default:
-                break;
-        }
-    }
-  }
