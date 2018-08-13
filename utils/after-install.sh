82f65a013f945854313e90cf1e9b17c3f8729db7
stsdc <stanislaw.dac@gmail.com>
2018-08-13 15:54:59 +0200

    build deb package with fpm

---
+++ utils/after-install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo pip3 install -r /opt/turtle/tcs/requirements.txt
+sudo ln -sf /opt/turtle/tcs/tcs /usr/bin/tcs
---
+++ utils/make_deb.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+fpm --input-type dir \
+	--output-type deb \
+	--maintainer "Kell ideas Ltd. <contact@turtlerover.com>" \
+	--name "tcs" \
+	--vendor "Kell ideas Ltd." \
+	--url "some url" \
+	--license "MIT" \
+	--version "0.12.1" \
+	--iteration 1 \
+	--architecture all \
+	--deb-no-default-config-files \
+	--exclude node_modules \
+    --exclude .git \
+    --exclude .cache \
+	--deb-dist stretch \
+	--verbose \
+	--url "https://github.com/TurtleRover/tcs" \
+	--depends "python3 (>= 3.5.3-1)" \
+	--depends "apache2" \
+	--prefix /opt/turtle/tcs \
+	--description "Turtle Rover Control Software"
+	--after-install after-install.sh
+	--after-remove after-remove.sh
+	.
