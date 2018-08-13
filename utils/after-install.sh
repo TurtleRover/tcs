f31b5d96ff504c030de0767342827979d7d0b08f
stsdc <stanislaw.dac@gmail.com>
2018-08-13 16:35:10 +0200

    install custom apache configs

--- utils/after-install.sh
+++ utils/after-install.sh
@@ -2,3 +2,15 @@
 
 sudo pip3 install -r /opt/turtle/tcs/requirements.txt
 sudo ln -sf /opt/turtle/tcs/tcs /usr/bin/tcs
+
+# Sorry for this T.T
+
+# Backup original apache configuration
+sudo mv /etc/apache2/apache2.conf /etc/apache2/apache2.conf.ORIG
+sudo mv /etc/apache2/sites-available /etc/apache2/sites-available.ORIG
+sudo mv /etc/apache2/sites-enabled /etc/apache2/sites-enabled.ORIG
+
+# Symlink custom apache configuration file
+sudo ln -sf /opt/turtle/tcs/apache_conf/apache2.conf /etc/apache2/apache2.conf
+sudo ln -sf /opt/turtle/tcs/apache_conf/sites-available /etc/apache2/sites-available
+sudo ln -sf /opt/turtle/tcs/apache_conf/sites-enabled /etc/apache2/sites-enabled
