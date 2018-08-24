1d9217ae9652a152033f8691f2bc5e78d3600684
stsdc <stanislaw.dac@gmail.com>
2018-08-24 13:19:21 +0200

    Add info about version to log

--- server/server.py
+++ server/server.py
@@ -8,7 +8,8 @@ from version import version_info
 logger = logname()
 
 def start_server():
-    logger.info('Starting Turtle Control System... [PID:%s PPID:%s]', os.getpid(), os.getppid())
+    logger.info('Turtle Control Software v' + version_info)
+    logger.info('[PID:%s PPID:%s]', os.getpid(), os.getppid())
     kill()
     logger.info('Starting new server instance...')
     # logger.info('Battery: %s', frame.readBatteryVoltage())
