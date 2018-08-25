daa986784955054f6d0dc523f01d1e302a4bf976
stsdc <stanislaw.dac@gmail.com>
2018-08-25 17:39:29 +0200

    add basic button class style

---
+++ client/src/js/view/components/elements/hold-button.js
@@ -0,0 +1,4 @@
+import { h } from 'hyperapp'
+
+export const HoldButton = ({state, value, text, setValue})  => 
+    <button class='button button-hold'>{text}</button>
--- client/src/js/view/components/settings/general.js
+++ client/src/js/view/components/settings/general.js
@@ -1,4 +1,8 @@
 import { h } from 'hyperapp'
+import { HoldButton } from '../elements/hold-button';
+
 
 export const SettingsGeneral = () =>
-    <div class="settings_content">General settings</div>
+    <div class="settings_content">
+        <HoldButton text='SHUTDOWN'></HoldButton>
+    </div>
