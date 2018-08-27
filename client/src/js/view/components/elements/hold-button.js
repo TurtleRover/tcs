901822c00644cdc316675e4c23397e44e64caa74
stsdc <stanislaw.dac@gmail.com>
2018-08-27 15:05:40 +0200

    add hold button logic

--- client/src/js/view/components/elements/hold-button.js
+++ client/src/js/view/components/elements/hold-button.js
@@ -1,4 +1,16 @@
 import { h } from 'hyperapp'
 
 export const HoldButton = ({state, value, text, setValue})  => 
-    <button class='button button-hold'>{text}</button>
+    <button 
+        class='button button-hold'
+        onmousedown={(event) => hold(setValue)}
+        onmouseup={(event) => clearTimeout(tid)}
+        onmouseleave={(event) => clearTimeout(tid)}>
+        {text}
+    </button>
+
+var tid;
+
+const hold = (cb) => {
+    tid = setTimeout(() => cb(), 200);   
+}
