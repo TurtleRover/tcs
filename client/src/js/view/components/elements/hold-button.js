c88d8dde0adf3e6c83047c9a75a17c8a5deb6a00
stsdc <stanislaw.dac@gmail.com>
2018-08-30 19:16:08 +0200

    add counter to hold button

--- client/src/js/view/components/elements/hold-button.js
+++ client/src/js/view/components/elements/hold-button.js
@@ -1,16 +1,47 @@
 import { h } from 'hyperapp'
+import { setInterval } from 'core-js';
 
 export const HoldButton = ({state, value, text, setValue})  => 
     <button 
         class='button button-hold'
-        onmousedown={(event) => hold(setValue)}
-        onmouseup={(event) => clearTimeout(tid)}
-        onmouseleave={(event) => clearTimeout(tid)}>
+        onmousedown={(event) => hold(event, setValue)}
+        onmouseup={(event) => clearTimers(event)}
+        onmouseleave={(event) => clearTimers(event)}>
+        <span class='button-hold_counter'>HOLD</span>
         {text}
     </button>
 
 var tid;
+var iid;
+var TIMEOUT = 4000;
 
-const hold = (cb) => {
-    tid = setTimeout(() => cb(), 200);   
+const hold = (event, cb) => {
+    event.target.children[0].textContent = TIMEOUT/1000;
+    countdown(event.target.children[0]);
+    tid = setTimeout(() => {
+        cb();
+        disable(event.target);
+        clearInterval(iid); 
+    }, TIMEOUT);
 }
+
+const countdown = (element) => {
+    let counter = 1;
+    iid = setInterval (()=> {
+        element.textContent = TIMEOUT/1000 - counter;
+        counter++;
+    }, 1000);
+}
+
+const clearTimers = (event) => {   
+    clearTimeout(tid);
+    clearInterval(iid);
+    event.target.children[0].textContent = 'HOLD';
+}
+
+const disable = (el) => {
+    el.disabled = true;
+    el.children[0].classList.add('button-hold_counter-disabled')
+    el.style.background = 'black';
+}
+    
--- client/src/js/view/components/settings/general.js
+++ client/src/js/view/components/settings/general.js
@@ -1,9 +1,15 @@
 import { h } from 'hyperapp'
 import { HoldButton } from '../elements/hold-button';
+import { System } from '../../../core/system';
 
 
 export const SettingsGeneral = ({actions}) =>
     <div class="settings_content">
-        <HoldButton text='SHUTDOWN' setValue={actions.system.shutdown}></HoldButton>
+        <HoldButton text='SHUTDOWN' setValue={() => actions.system.shutdown()}></HoldButton>
     </div>
 
+const lol = (a) => {
+    console.log('-----------',a);
+    a.shutdown();
+    
+}
