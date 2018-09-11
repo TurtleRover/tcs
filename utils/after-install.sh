59f2522a2301b86cbe9fe8170784abea9da2993a
stsdc <stanislaw.dac@gmail.com>
2018-09-11 12:53:28 +0200

    extend pip3 timeout

--- utils/after-install.sh
+++ utils/after-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pip3 install -r /opt/turtlerover/tcs/requirements.txt
+sudo pip3 install --default-timeout=100 -r /opt/turtlerover/tcs/requirements.txt
 sudo ln -sf /opt/turtlerover/tcs/tcs /usr/bin/tcs
 
 sudo cp /opt/turtlerover/tcs/server/turtlerover-tcs.service /etc/systemd/system
