8a440b98ed77521513e20f829366bddf6f0a876f
stsdc <stanislaw.dac@gmail.com>
2018-09-13 22:01:27 +0200

    statusbar → topbar

--- client/src/js/view/components/topbar.js
+++ client/src/js/view/components/topbar.js
@@ -1,31 +1,31 @@
 import { h } from 'hyperapp'
 
 
-export const StatusBar = ({state, switchSettings})  => 
-    <section id="statusbar" class="statusbar">
-        <img class="statusbar_logo" src={require("../../../img/ui/turtle-logo.svg")}/>
-        <div class="statusbar_indicators">
+export const TopBar = ({state, switchSettings})  => 
+    <section id="topbar" class="topbar">
+        <img class="topbar_logo" src={require("../../../img/ui/turtle-logo.svg")}/>
+        <div class="topbar_indicators">
             <IndicatorBattery batteryLevel={state.batteryLevel}/>
             <IndicatorSignal signalLevel={state.signalLevel}/>
         </div>
-        <div id="statusbar_actions" class="statusbar_actions">
-            {/* <img class="statusbar_actions_action" id="button-screenrecord" src={require("../../../img/ui/nav-bar-rec.svg")}/> */}
+        <div id="topbar_actions" class="topbar_actions">
+            {/* <img class="topbar_actions_action" id="button-screenrecord" src={require("../../../img/ui/nav-bar-rec.svg")}/> */}
                 {/* <a id="snap-download-a">  */}
-            {/* <img class="statusbar_actions_action" id="button-screenshot" src={require("../../../img/ui/nav-bar-snap.svg")}/> */}
+            {/* <img class="topbar_actions_action" id="button-screenshot" src={require("../../../img/ui/nav-bar-snap.svg")}/> */}
                 {/* </a>  */}
-            {/* <img class="statusbar_actions_action" id="button-fullscreen" src={require("../../../img/ui/nav-bar-fullscreen.svg")}/> */}
+            {/* <img class="topbar_actions_action" id="button-fullscreen" src={require("../../../img/ui/nav-bar-fullscreen.svg")}/> */}
         </div>
 
-        <div class="statusbar_menu" onmousedown={() => switchSettings()}>
-            <div class="statusbar_menu_bar"></div>
+        <div class="topbar_menu" onmousedown={() => switchSettings()}>
+            <div class="topbar_menu_bar"></div>
         </div>
     </section>
 
 const IndicatorBattery = ({batteryLevel}) => 
-    <img id="indicator-battery" class="statusbar_indicators_indicator" src={batteryLevelIcon(batteryLevel)}/>
+    <img id="indicator-battery" class="topbar_indicators_indicator" src={batteryLevelIcon(batteryLevel)}/>
 
 const IndicatorSignal = ({signalLevel}) =>
-    <img id="indicator-signal" class="statusbar_indicators_indicator" src={signalLevelIcon(signalLevel)}/>
+    <img id="indicator-signal" class="topbar_indicators_indicator" src={signalLevelIcon(signalLevel)}/>
 
 
 const batteryLevelIcon = (batteryLevel) => {
