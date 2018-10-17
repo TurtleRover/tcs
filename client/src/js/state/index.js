feb1c0dcde0d9e317fde510531bddc7c5e5786bc
Frozen <mrozix11@gmail.com>
2018-10-17 11:51:00 +0200

    Update index.js

--- client/src/js/state/index.js
+++ client/src/js/state/index.js
@@ -10,22 +10,22 @@ let state = {
     mode: 'drive',
     manipulator: {
         axis1: {
-            min: 2200,
-            max: 5000,
+            min: 2800,
+            max: 4850,
             step: 10,
-            value: 4600
+            value: 3300
         },
         axis2: {
             min: 2700,
-            max: 5000,
+            max: 4450,
             step: 10,
-            value: 3400
+            value: 3300
         },
         gripper: {
             min: 2400,
             max: 3600,
             step: 10,
-            value: 3000
+            value: 3300
         },
     },
     
@@ -39,5 +39,5 @@ let state = {
         temperature: ""
     }
 };
-
+ 
 export default state;
