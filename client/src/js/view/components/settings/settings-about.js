f78ab2bc485be4051db719fdb02d497279ad43d9
stsdc <stanislaw.dac@gmail.com>
2018-10-19 16:49:38 +0200

    Add "Shield" to Firmware string

--- client/src/js/view/components/settings/settings-about.js
+++ client/src/js/view/components/settings/settings-about.js
@@ -1,9 +1,9 @@
-import { h } from 'hyperapp'
+import { h } from 'hyperapp';
 
-export const SettingsAbout = ({state}) =>
+export const SettingsAbout = ({ state }) =>
     <div class="settings_content">
         <div>Turtle Control Software v{state.system_info.tcs_ver}</div>
-        <div>Firmware v{state.system_info.firmware_ver}</div>
+        <div>Shield Firmware v{state.system_info.firmware_ver}</div>
         <div>External WiFi: {state.system_info.wifi_dongle}</div>
         <div>Video devices: {() => state.system_info.video_devices.toString()}</div>
-    </div>
+    </div>;
