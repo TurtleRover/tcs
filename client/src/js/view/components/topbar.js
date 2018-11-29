d154c836305afd7e57fd9344c9bfdf79bdfa37c3
stsdc <stanislaw.dac@gmail.com>
2018-11-29 18:32:32 +0100

    fix some eslint errors

--- client/src/js/view/components/topbar.js
+++ client/src/js/view/components/topbar.js
@@ -74,8 +74,14 @@ const toggleFullscreen = (event) => {
 
     const isFullscreen = document.webkitIsFullScreen || document.mozFullScreen || false;
 
-    element.requestFullScreen = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || function () { return false; };
-    document.cancelFullScreen = document.cancelFullScreen || document.webkitCancelFullScreen || document.mozCancelFullScreen || function () { return false; };
+    element.requestFullScreen = element.requestFullScreen
+        || element.webkitRequestFullScreen
+        || element.mozRequestFullScreen
+        || function a() { return false; };
+    document.cancelFullScreen = document.cancelFullScreen
+        || document.webkitCancelFullScreen
+        || document.mozCancelFullScreen
+        || function a() { return false; };
 
     isFullscreen ? document.cancelFullScreen() : element.requestFullScreen();
 };
