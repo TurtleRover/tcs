d43f1f04ed38ca09868ffe20a4c51e3cf608d1ac
stsdc <stanislaw.dac@gmail.com>
2018-12-13 18:36:37 +0100

    make settings sidebar scrollable

--- client/src/js/view/components/settings/index.js
+++ client/src/js/view/components/settings/index.js
@@ -7,22 +7,22 @@ import { SettingsPreprogram } from './settings-preprogram';
 import { SettingsClupi } from './settings-clupi';
 import { SettingsAbout } from './settings-about';
 
-export const Settings = ({ state, actions }) =>
-    <div class={(state.settings.isVisible === true) ? 'settings' : 'settings settings-hide'}>
-        <div class="settings_categories">
-            <div role='button' class={categoryClass(state.settings.category, 'general')} onmousedown={() => actions.settings.setVisibleCategory('general')}>General</div>
-            {/* <li class={categoryClass(state.settings.category, 'network')} onmousedown={() => actions.settings.setVisibleCategory('network')}>Network</li> */}
-            <div role='button' class={categoryClass(state.settings.category, 'manipulator')} onmousedown={() => actions.settings.setVisibleCategory('manipulator')}>Manipulator</div>
-            <div role='button' class={categoryClass(state.settings.category, 'preprogram')} onmousedown={() => actions.settings.setVisibleCategory('preprogram')}>Pre-program</div>
-            {/* <li class={categoryClass(state.settings.category, 'debug')} onmousedown={() => actions.settings.setVisibleCategory('debug')}>Debug</li> */}
-            <div role='button' class={categoryClass(state.settings.category, 'clupi')} onmousedown={() => actions.settings.setVisibleCategory('clupi')}>Experimental</div>
-            <div role='button' class={categoryClass(state.settings.category, 'about')} onmousedown={() => actions.settings.setVisibleCategory('about')}>About</div>
-        </div>
-        <SettingsContent state={state} actions={actions} />
-    </div>;
+// export const Settings = ({ state, actions }) =>
+//     <div class={(state.settings.isVisible === true) ? 'settings' : 'settings settings-hide'}>
+//         <div class="settings_categories">
+//             <div role='button' class={categoryClass(state.settings.category, 'general')} onmousedown={() => actions.settings.setVisibleCategory('general')}>General</div>
+//             <li class={categoryClass(state.settings.category, 'network')} onmousedown={() => actions.settings.setVisibleCategory('network')}>Network</li>
+//             <div role='button' class={categoryClass(state.settings.category, 'manipulator')} onmousedown={() => actions.settings.setVisibleCategory('manipulator')}>Manipulator</div>
+//             <div role='button' class={categoryClass(state.settings.category, 'preprogram')} onmousedown={() => actions.settings.setVisibleCategory('preprogram')}>Pre-program</div>
+//             <li class={categoryClass(state.settings.category, 'debug')} onmousedown={() => actions.settings.setVisibleCategory('debug')}>Debug</li>
+//             <div role='button' class={categoryClass(state.settings.category, 'clupi')} onmousedown={() => actions.settings.setVisibleCategory('clupi')}>Experimental</div>
+//             <div role='button' class={categoryClass(state.settings.category, 'about')} onmousedown={() => actions.settings.setVisibleCategory('about')}>About</div>
+//         </div>
+//         <SettingsContent state={state} actions={actions} />
+//     </div>;
 
 
-const SettingsContent = ({ state, actions }) => {
+const SettingContent = ({ state, actions }) => {
     switch (state.settings.category) {
         case 'general':
             return <SettingsGeneral actions={actions} />;
@@ -43,9 +43,24 @@ const SettingsContent = ({ state, actions }) => {
     }
 }
 
-const categoryClass = (state, category) => {
-    if (state === category) {
-        return 'settings_categories_category settings_categories_category-selected';
-    }
-    return 'settings_categories_category';
-};
+const selectedClass = (isSelected) => (isSelected ? ' settings__settingButton--selected' : '');
+
+const SettingButton = ({ state, actions, name }) =>
+    <div
+        role='button'
+        class={'settings__settingButton' + selectedClass(state.settings.category === name)}
+        onmousedown={() => actions.settings.setVisibleCategory(name)}>
+        {name.toUpperCase()}
+    </div>;
+
+export const Settings = ({ state, actions }) =>
+    <div class={(state.settings.isVisible === true) ? 'settings' : 'settings settings-hide'}>
+        <div class="settings__sidebar">
+            <SettingButton state={state} actions={actions} name='general' />
+            <SettingButton state={state} actions={actions} name='manipulator' />
+            <SettingButton state={state} actions={actions} name='preprogram' />
+            <SettingButton state={state} actions={actions} name='clupi' />
+            <SettingButton state={state} actions={actions} name='about' />
+        </div>
+        <SettingContent state={state} actions={actions} />
+    </div>;
