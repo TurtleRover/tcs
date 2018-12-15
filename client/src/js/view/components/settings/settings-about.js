02587a47814af2a948a3c6f9ce1216a3690d56d6
stsdc <stanislaw.dac@gmail.com>
2018-12-15 01:42:59 +0100

    remove commented code

--- client/src/js/view/components/settings/settings-about.js
+++ client/src/js/view/components/settings/settings-about.js
@@ -23,16 +23,5 @@ export const SettingsAbout = ({ state }) =>
         <div class="settings__section"><Link href='www.turtlerover.com' text='www.turtlerover.com' /></div>
     </div>;
 
-
-    //     <p>
-    //         
-    //         Feel free to contact us if you have any issue. We're constantly improving the UI, but it's still a long way to go.
-    //         We'd love to have you participate in the project and get any feedback!
-    //     </p>
-    //     <span>Turtle Team</span>
-    //     <span>contact@turtlerover.com</span>
-    //     <span>www.turtlerover.com</span>
-    // </div>;
-
 const VideoDevice = ({ device }) =>
     <span>Video device: {device.model} at {device.device}</span>;
