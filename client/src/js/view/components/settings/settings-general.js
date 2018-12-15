66f3abfab2a8c7db3806d7813e3525004b404874
stsdc <stanislaw.dac@gmail.com>
2018-12-15 01:52:45 +0100

    relayout general setting

--- client/src/js/view/components/settings/settings-general.js
+++ client/src/js/view/components/settings/settings-general.js
@@ -2,8 +2,15 @@ import { h } from 'hyperapp';
 import { ButtonHold } from '../elements/button-hold';
 
 export const SettingsGeneral = ({ actions }) =>
-    <div class="settings_content">
-        <ButtonHold text='SHUTDOWN' setValue={() => actions.system.shutdown()} />
-        <ButtonHold text='REBOOT' setValue={() => actions.system.reboot()} />
-        <ButtonHold text='Restart stream' setValue={() => actions.stream.start()} />
+    <div class="settings__block">
+        <div class='settings__subtitle'>system actions</div>
+        <div class="settings__section">
+            <ButtonHold text='SHUTDOWN' setValue={() => actions.system.shutdown()} />
+        </div>
+        <div class="settings__section">
+            <ButtonHold text='REBOOT' setValue={() => actions.system.reboot()} />
+        </div>
+        <div class="settings__section">
+            <ButtonHold text='Restart stream' setValue={() => actions.stream.start()} />
+        </div>
     </div>;
