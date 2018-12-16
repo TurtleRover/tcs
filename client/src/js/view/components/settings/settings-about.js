c89348f8e0c52e2e678b5659ff99d98ad25ca55d
stsdc <stanislaw.dac@gmail.com>
2018-12-17 00:21:08 +0100

    separate setting blocks with a border

--- client/src/js/view/components/settings/settings-about.js
+++ client/src/js/view/components/settings/settings-about.js
@@ -2,25 +2,29 @@ import { h } from 'hyperapp';
 import { Link } from '../elements/link';
 
 export const SettingsAbout = ({ state }) =>
-    <div class='settings__block'>
-        <div class='settings__subtitle'>system info</div>
-        <div class="settings__section">Turtle Control Software v{state.system_info.tcs_ver}</div>
-        <div class="settings__section">Shield Firmware v{state.system_info.firmware_ver}</div>
-        <div class="settings__section">External WiFi: {state.system_info.wifi_dongle}</div>
-        <div class="settings__section">{state.system_info.video_devices.map(device => (<VideoDevice device={device} />))}</div>
-
-        <div class='settings__subtitle'>Documentation</div>
-        <div class="settings__section">
-            Please refer to &nbsp;<Link href='https://docs.turtlerover.com/' text='docs.turtlerover.com' /> &nbsp;to know more about the control options and the Rover maintenance.
+    <div class='settings__content'>
+        <div class='settings__block'>
+            <div class='settings__subtitle'>system info</div>
+            <div class="settings__section">Turtle Control Software v{state.system_info.tcs_ver}</div>
+            <div class="settings__section">Shield Firmware v{state.system_info.firmware_ver}</div>
+            <div class="settings__section">External WiFi: {state.system_info.wifi_dongle}</div>
+            <div class="settings__section">{state.system_info.video_devices.map(device => (<VideoDevice device={device} />))}</div>
         </div>
-
-        <div class='settings__subtitle'>A word from a team</div>
-        <div class="settings__section">
-            Feel free to contact us if you have any issue. We're constantly improving the UI, but it's still a long way to go.<br />
-            We'd love to have you participate in the project and get any feedback!
+        <div class='settings__block'>
+            <div class='settings__subtitle'>Documentation</div>
+            <div class="settings__section">
+                Please refer to &nbsp;<Link href='https://docs.turtlerover.com/' text='docs.turtlerover.com' /> &nbsp;to know more about the control options and the Rover maintenance.
+            </div>
         </div>
-        <div class="settings__section"><Link href='mailto:contact@turtlerover.com' text='contact@turtlerover.com' /></div>
-        <div class="settings__section"><Link href='www.turtlerover.com' text='www.turtlerover.com' /></div>
+        <div class='settings__block'>
+            <div class='settings__subtitle'>A word from a team</div>
+            <div class="settings__section">
+                Feel free to contact us if you have any issue. We're constantly improving the UI, but it's still a long way to go.<br />
+                We'd love to have you participate in the project and get any feedback!
+            </div>
+            <div class="settings__section"><Link href='mailto:contact@turtlerover.com' text='contact@turtlerover.com' /></div>
+            <div class="settings__section"><Link href='www.turtlerover.com' text='www.turtlerover.com' /></div>
+        </div>
     </div>;
 
 const VideoDevice = ({ device }) =>
